7a1cabe02c11f0b410cf97a8018ae378fce4520c
Michelle Livengood <ml2876@nyu.edu>
2018-02-22 16:12:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-'use strict';
 
-function theBeatlesPlay(musicians, instruments) {
-  var phrases = [];
-  for (var i=0; i<musicians.length; i++) {
-    phrases.push(musicians[i] + "plays" + instruments[i]);
+'use strict';
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+  var together = [];
+  for (var i = 0; i < musicians.length; i++){
+    var sentence = musicians[i] + " plays " + instruments[i];
+    together.push(sentence);
   }
-  return phrases;
+return together;
 }
